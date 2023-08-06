db7d474b7a2ce87964c8ab52383ead81e1faed5e
gldaniil <daniil.gajfullin@yandex.ru>
2023-08-06 21:06:37 +0500

    new component: Drawer

---
+++ src/components/Drawer.js
@@ -0,0 +1,52 @@
+const Drawer = () => {
+  return (
+    <div className="overlay" style={{ display: "none" }}>
+      <div className="drawer">
+        <h2 className="d-flex justify-between mb-30">
+          Корзина{" "}
+          <img
+            src="/img/button-remove.svg"
+            alt="Remove"
+            className="cart__remove-button cu-p"
+          />
+        </h2>
+        <div className="items">
+          <div className="cart__item d-flex align-center mb-20">
+            <div
+              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+              className="cart__item-img"
+            />
+            <div className="mr-20 flex">
+              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img
+              src="/img/button-remove.svg"
+              alt="Remove"
+              className="cart__remove-button"
+            />
+          </div>
+        </div>
+        <div className="cartTotalBlock">
+          <ul>
+            <li className="d-flex">
+              <span>Итого:</span>
+              <div></div>
+              <b>21 498 руб.</b>
+            </li>
+            <li className="d-flex">
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>1074 руб.</b>
+            </li>
+          </ul>
+          <button className="green-button">
+            Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Drawer;
