15228ddd0870d03ce0e5704deb97c9b713ff3a03
gldaniil <daniil.gajfullin@yandex.ru>
2023-08-06 19:31:07 +0500

    overlay cart ready

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2 className="mb-30">Корзина</h2>
+          <h2 className="d-flex justify-between mb-30">
+            Корзина{" "}
+            <img
+              src="/img/button-remove.svg"
+              alt="Remove"
+              className="cart__remove-button cu-p"
+            />
+          </h2>
           <div className="items">
             <div className="cart__item d-flex align-center mb-20">
               <div
@@ -20,6 +27,23 @@ function App() {
                 className="cart__remove-button"
               />
             </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li className="d-flex">
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб.</b>
+              </li>
+              <li className="d-flex">
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб.</b>
+              </li>
+            </ul>
+            <button className="green-button">
+              Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
+            </button>
           </div>
         </div>
       </div>
