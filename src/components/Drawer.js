e9bef24ec05842b71e9779acf2500db5b7337a5c
gldaniil <daniil.gajfullin@yandex.ru>
2023-08-10 17:51:28 +0500

    update: UI empty cart / cart with sneakers

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -12,59 +12,66 @@ const Drawer = ({ onClose, onRemove, items = [] }) => {
           />
         </h2>
 
-        <div className="cartEmpty d-flex align-center justify-center flex-column flex">
-          <img
-            className="mb-20"
-            width={120}
-            height={120}
-            src="/img/empty-cart.png"
-            alt="Empty cart"
-          />
-          <h2>Корзина пустая</h2>
-          <p>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</p>
-          <button className="green-button">
-            <img src="/img/arrow.svg" alt="Arrow" />
-            Вернуться назад
-          </button>
-        </div>
-
-        <div className="items">
-          {items.map((obj, index) => (
-            <div key={index} className="cart__item d-flex align-center mb-20">
-              <div
-                style={{ backgroundImage: `url(${obj.image})` }}
-                className="cart__item-img"
-              />
-              <div className="mr-20 flex">
-                <p className="mb-5">{obj.title}</p>
-                <b>{obj.price} руб.</b>
-              </div>
-              <img
-                onClick={() => onRemove(obj.id)}
-                src="/img/button-remove.svg"
-                alt="Remove"
-                className="cart__remove-button"
-              />
+        {items.length > 0 ? (
+          <>
+            <div className="items">
+              {items.map((obj, index) => (
+                <div
+                  key={index}
+                  className="cart__item d-flex align-center mb-20"
+                >
+                  <div
+                    style={{ backgroundImage: `url(${obj.image})` }}
+                    className="cart__item-img"
+                  />
+                  <div className="mr-20 flex">
+                    <p className="mb-5">{obj.title}</p>
+                    <b>{obj.price} руб.</b>
+                  </div>
+                  <img
+                    onClick={() => onRemove(obj.id)}
+                    src="/img/button-remove.svg"
+                    alt="Remove"
+                    className="cart__remove-button"
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <div className="cartTotalBlock">
-          <ul>
-            <li className="d-flex">
-              <span>Итого:</span>
-              <div></div>
-              <b>21 498 руб.</b>
-            </li>
-            <li className="d-flex">
-              <span>Налог 5%:</span>
-              <div></div>
-              <b>1074 руб.</b>
-            </li>
-          </ul>
-          <button className="green-button">
-            Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
-          </button>
-        </div>
+            <div className="cartTotalBlock">
+              <ul>
+                <li className="d-flex">
+                  <span>Итого:</span>
+                  <div></div>
+                  <b>21 498 руб.</b>
+                </li>
+                <li className="d-flex">
+                  <span>Налог 5%:</span>
+                  <div></div>
+                  <b>1074 руб.</b>
+                </li>
+              </ul>
+              <button className="green-button">
+                Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="cartEmpty d-flex align-center justify-center flex-column flex">
+            <img
+              className="mb-20"
+              width={120}
+              height={120}
+              src="/img/empty-cart.png"
+              alt="Empty cart"
+            />
+            <h2>Корзина пустая</h2>
+            <p>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</p>
+            <button onClick={onClose} className="green-button">
+              <img src="/img/arrow.svg" alt="Arrow" />
+              Вернуться назад
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
