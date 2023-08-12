98039de3367a340c427968247cc1abb375fe0dec
gldaniil <daniil.gajfullin@yandex.ru>
2023-08-12 17:28:17 +0500

    added: <Link /> from react-router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,26 @@
+import { Link } from "react-router-dom";
+
 const Header = (props) => {
   return (
     <header className="d-flex justify-between align-center p-40">
-      <div className="d-flex align-center">
-        <img width={40} height={40} alt="Logo" src="img/logo.png" />
-        <div>
-          <h3 className="text-uppercase">React Sneakers</h3>
-          <p className="opacity-5">Магазин лучших кроссовок</p>
+      <Link to="/">
+        <div className="d-flex align-center">
+          <img width={40} height={40} alt="Logo" src="img/logo.png" />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p className="opacity-5">Магазин лучших кроссовок</p>
+          </div>
         </div>
-      </div>
+      </Link>
       <ul className="navMenu d-flex">
         <li onClick={props.onClickCart} className="mr-30 cu-p">
           <img src="/img/backet.svg" alt="Backet" />
           <span>1205 руб.</span>
         </li>
         <li className="mr-20 cu-p">
-          <img src="/img/liked.svg" alt="Liked" />
+          <Link to="/favorites">
+            <img src="/img/liked.svg" alt="Liked" />
+          </Link>
         </li>
         <li className="cu-p">
           <img src="/img/profile.svg" alt="Profile" />
